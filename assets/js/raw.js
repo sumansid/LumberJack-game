f31e4f496806542f4a3f155cf05e60699156ee59
Suman Sigdel <53033648+sumansid@users.noreply.github.com>
2020-05-03 15:25:11 -0400

    Update raw.js

--- assets/js/raw.js
+++ assets/js/raw.js
@@ -2,6 +2,6 @@ Facts = [
 	['Evaporation of water from trees has a cooling influence.'],
 	['In British Columbia, Canada, more than 80 wildlife species depend on trees.'],
 	['Global forests removed about one-third of fossil fuel emissions annually from 1990 to 2007.'],
-	['During one year, a mature tree will absorb more than 48 pounds of carbon dioxide from the atmosphere and release oxygen in exchange.']
-
+	['During one year, a mature tree will absorb more than 48 pounds of carbon dioxide from the atmosphere and release oxygen in exchange.'],
+	['A single tree produces approximately 260 pounds of oxygen per year.']
 ];
